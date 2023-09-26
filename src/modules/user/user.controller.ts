4f92e143e43c8942ef963467fd0d5f123b4b677d
Hisrael Braga <hisraelc@gmail.com>
2023-09-26 00:57:06 -0400

    Refactor: added name, email and password validation

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { BadRequestException, Controller, Post, Req } from '@nestjs/common';
 import { UsersService } from './user.service';
 import { User } from '@prisma/client';
 import { createUserDTO } from './user.dto';
@@ -14,6 +14,18 @@ export class UserController {
     async createUser(@Req() request: Request): Promise<User> {
         try {
             const {name, email, password} = request.body;
+
+            if(!name || typeof(name) !== 'string' || name.length === 0) {
+                throw new BadRequestException('Name is required');
+            }
+
+            if(!email || typeof(email) !== 'string' || email.length === 0) {
+                throw new BadRequestException('Email is required');
+            }
+
+            if(!password || typeof(password) !== 'string' || password.length === 0) {
+                throw new BadRequestException('Password is required');
+            }
 
             return await this.userService.createUser({name, email, password});
         } catch (error) {
