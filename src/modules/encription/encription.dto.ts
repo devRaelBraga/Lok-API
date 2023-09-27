253cab8a48fcd56a48f0f790344f1cce74fffa0a
Hisrael Braga <hisraelc@gmail.com>
2023-09-27 00:31:28 -0400

    Feat: added basic structure of EncriptionService

---
+++ src/modules/encription/encription.dto.ts
@@ -0,0 +1,10 @@
+export interface CriptMessageDTO {
+    message: string,
+    key:string
+}
+
+export interface DeCriptMessageDTO {
+    message: string,
+    key:string
+}
+
--- src/modules/encription/encription.service.ts
+++ src/modules/encription/encription.service.ts
@@ -1,5 +1,11 @@
+import { CriptMessageDTO, DeCriptMessageDTO } from "./encription.dto";
 
 
 export class EncriptionService {
-    
+    async criptMessage({message, key}: CriptMessageDTO): Promise<string> {
+        return message;
+    }
+    async deCriptMessage({message, key}: DeCriptMessageDTO): Promise<string> {
+        return message;
+    }
 }
