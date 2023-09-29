3d2bc8e7379f6b44a31b4e238e31ae7cd0bd497d
Hisrael Braga <hisraelc@gmail.com>
2023-09-29 12:16:20 -0400

    Fix: removed unused appController and appService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MessageModule } from './modules/message/messages.module';
 import { UsersModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [AuthModule, MessageModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
