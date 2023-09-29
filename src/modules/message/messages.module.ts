b41db050bc58d0d45fcb1f300e27c5e1e5d93c0d
Hisrael Braga <hisraelc@gmail.com>
2023-09-29 12:17:06 -0400

    Fix: added services on MessageModule

--- src/modules/message/messages.module.ts
+++ src/modules/message/messages.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { EncriptionService } from '../encription/encription.service';
 import { MessageController } from './message.controller';
+import { PrismaService } from 'config/database/prisma.service';
+import { UsersService } from '../user/user.service';
 
 @Module({
   controllers: [MessageController],
-  providers: [MessageService, EncriptionService],
+  providers: [MessageService, EncriptionService, PrismaService, UsersService],
   exports: [],
 })
 export class MessageModule {}
